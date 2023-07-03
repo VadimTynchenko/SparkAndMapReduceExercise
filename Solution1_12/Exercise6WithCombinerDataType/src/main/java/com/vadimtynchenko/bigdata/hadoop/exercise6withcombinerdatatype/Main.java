0332da6a5de39f75598f4973a79c21aa518be9e7
vadimtynchenko <tynchenko.vadim@gmail.com>
2023-07-03 18:19:51 +0400

    Initial commit

---
+++ Solution1_12/Exercise6WithCombinerDataType/src/main/java/com/vadimtynchenko/bigdata/hadoop/exercise6withcombinerdatatype/Main.java
@@ -0,0 +1,7 @@
+package com.vadimtynchenko.bigdata.hadoop.exercise6withcombinerdatatype;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
