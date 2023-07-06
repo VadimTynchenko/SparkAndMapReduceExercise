45ef0a79a22af4cbbe8ecf3f7926c57c61ce7afc
vadimtynchenko <tynchenko.vadim@gmail.com>
2023-07-06 18:05:11 +0400

    Initial Commit

---
+++ Solution1_12/Exercise8SingleJob/src/main/java/com/vadimtynchenko/bigdata/hadoop/exercise8singlejob/Main.java
@@ -0,0 +1,7 @@
+package com.vadimtynchenko.bigdata.hadoop.exercise8singlejob;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
